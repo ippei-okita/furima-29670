90f76cd3f2c7ea5dfa3cf4f57606448c9b719f71
ippei-okita <okpei1985@gmail.com>
2020-09-23 18:11:32 +0900

    userモデルのテストコード完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def create
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   with_options presence: true do
     validates :nickname
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
     validates :birth_day
-    validates :email,    uniqueness: { case_sensitive: false },
-                         format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :password, length: { minimum: 6 }
   end
-
-  with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
-    validates :family_name, presence: true
-    validates :first_name, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :family_name
+    validates :first_name
   end
-
-  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :family_kana, presence: true
-    validates :first_kana, presence: true
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :family_kana
+    validates :first_kana
   end
 end
