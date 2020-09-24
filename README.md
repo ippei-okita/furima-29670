8fee8238e736b47290444f6c59402bb645660d88
ippei-okita <okpei1985@gmail.com>
2020-09-24 23:37:46 +0900

    無関係なコード削除

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 | ----------- | ------ | ------------------------- |
 | nickname    | string | null: false               |
 | email       | string | null: false, unique: true |
-| password    | string | null: false, unique: true |
+| password    | string | null: false               |
 | family_name | string | null: false               |
 | first_name  | string | null: false               |
 | family_kana | string | null: false               |
--- db/migrate/20200921051826_devise_create_users.rb
+++ db/migrate/20200921051826_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,           null: false, default: ""
       t.string :email,              null: false, default: "", unique: true
-      t.string :encrypted_password, null: false, default: "", unique: true
+      t.string :encrypted_password, null: false, default: ""
       t.string :family_name,        null: false
       t.string :first_name,         null: false
       t.string :family_kana,        null: false
