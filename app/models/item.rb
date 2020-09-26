32c63fd980c50f98eeee1ae5142ffd1c6d4413ff
ippei-okita <okpei1985@gmail.com>
2020-09-26 17:33:26 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
+  
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 20 }
+    validates :description, length: { maximum: 1000 }
+    validates :category_id
+    validates :condition_id
+    validates :shipping_id
+    validates :prefecture_id
+    validates :day_id
+    validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
