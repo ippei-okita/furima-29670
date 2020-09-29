1465d331c0f749cf173607dbf30619fa2d88dc62
ippei-okita <okpei1985@gmail.com>
2020-09-29 12:02:44 +0900

    Rubocop実行後

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 1) }
-    email                 {Faker::Internet.free_email}
+    email                 { Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name           {'大北'}
