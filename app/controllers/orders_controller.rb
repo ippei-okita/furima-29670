27aa3b80c22704347f4cb422736fcf1746bf09f7
ippei-okita <okpei1985@gmail.com>
2020-09-30 17:47:07 +0900

    商品購入ページに遷移できる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,14 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = Order.new
+    @item = Item.find(10)
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
-    else
-    render :index
-    end
+    
   end
 
   private
 
-  def order_params
-    params.require(:order).permit(:user_id, :item_id)
-
+  
 end
