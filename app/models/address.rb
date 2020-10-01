66976fbc32c6e0fd03f315d91f9c0a6aba4ed408
ippei-okita <okpei1985@gmail.com>
2020-10-01 12:11:49 +0900

    addressモデルのバリデーション追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,14 +3,14 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   with_options presense: true do
-    validates :postal_code
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id
-    validates :city
-    validates :address
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :address, format: { with: /\A[ぁ-んァ-ン一-龥]+[0-9０-９]+\z/ }
   end
 
-  validates :phene_num, presense: true, uniqueness: true
+  validates :phene_num, presense: true, uniqueness: true, format: { with: /\A\d{11}\z/ }
   validates :order, presense: true
-  
+
   end
 end
