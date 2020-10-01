8a533d6dccd958c3e36197acf570af006bfa0e09
ippei-okita <okpei1985@gmail.com>
2020-10-01 11:50:34 +0900

    addressモデルのバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :user
+  belongs_to_active_hash :prefecture
+
+  with_options presense: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :address
+  end
+
+  validates :phene_num, presense: true, uniqueness: true
+  validates :order, presense: true
+  
+  end
 end
