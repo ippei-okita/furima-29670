f9aeac1f328e76cd29931b8d8c95fd881613409d
ippei-okita <okpei1985@gmail.com>
2020-10-03 21:14:40 +0900

    order_addressモデルのFactoryBot

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code            {123-4567}
+    prefecture_id          {Faker::Number.within(range: 2..48)}
+    city                   {Faker::Address.street_name}
+    address                {Faker::Address.street_address}
+    building               {Faker::Address.building_number}
+    phone_num              {Faker::Number.number(digits: 11)}
+    token                  {Faker::Lorem.characters}
+    
+  end
+end
