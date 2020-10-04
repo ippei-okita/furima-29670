03a8ad1852f2e25709e3da8c9485f39234fa3812
ippei-okita <okpei1985@gmail.com>
2020-10-04 09:48:56 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ipewo' && password == '0412'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :family_kana, :first_kana, :birth_day])
